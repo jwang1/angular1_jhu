6a2e230ececdc3bdb1785cbea6375c91f6208b21
jwang1 <sigkdd@yahoo.com>
2017-05-14 16:26:52 -0400

    Minification

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 (function(angular) {
     'use strict';
 
@@ -7,6 +8,9 @@
     // the following mistaken put DiController DI; caused the error:
     //      Error: [$injector:unpr] Unknown provider: DiControllerProvider <- DiController <- DiController
     // function DiController($scope, $filter, DiController) {
+
+    DiController.$inject = ['$scope', '$filter', '$injector'];
+
     function DiController($scope, $filter, $injector) {
         $scope.name = 'Joe';
 
@@ -23,3 +27,12 @@
     }
 
 })(window.angular);
+*/
+
+//  https://javascript-minifier.com/
+// after adding line of DiController.$inject = ['$scope', '$filter', '$injector']
+!function(o){"use strict";function e(o,n,t){o.name="Joe",o.showUpperCase=function(){var e=n("uppercase");o.name=e(o.name)},console.log(t.annotate(e)),console.log(e)}o.module("DiApp",[]).controller("DiController",e),e.$inject=["$scope","$filter","$injector"]}(window.angular);
+
+
+// https://javascript-minifier.com/  result not handling well for Angular $ ...
+// !function(o){"use strict";function n(o,e,a){o.name="Joe",o.showUpperCase=function(){var n=e("uppercase");o.name=n(o.name)},console.log(a.annotate(n)),console.log(n)}o.module("DiApp",[]).controller("DiController",n)}(window.angular);
