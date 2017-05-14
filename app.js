f7918c019a9e55f91073666bcaf877687b314072
jwang1 <sigkdd@yahoo.com>
2017-05-14 01:16:16 -0400

    AngularJS IoC

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-(function() {
+(function(angular) {
+    'use strict';
 
-})();
+    angular.module('DiApp', [])
+    .controller('DiController', DiController);
+
+    // the following mistaken put DiController DI; caused the error:
+    //      Error: [$injector:unpr] Unknown provider: DiControllerProvider <- DiController <- DiController
+    // function DiController($scope, $filter, DiController) {
+    function DiController($scope, $filter, $injector) {
+        $scope.name = 'Joe';
+
+        $scope.showUpperCase = function() {
+            var upCaseFunc = $filter('uppercase');
+
+            $scope.name = upCaseFunc($scope.name);
+        };
+
+        //console.log($injector.log(DiController.toString()));
+        // $injector.log(DiController);
+        console.log($injector.annotate(DiController));
+        console.log(DiController);
+    }
+
+})(window.angular);
