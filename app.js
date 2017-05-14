12190f59906e9aaa55ba29e63807509cb59cb480
jwang1 <sigkdd@yahoo.com>
2017-05-14 00:01:43 -0400

    AngularJS under the hood

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-(function(angular) {
-    'use strict';
+(function() {
 
-    angular.module('myCalculator', [])
-    .controller('myCalculatorController', function($scope) {
-
-        $scope.name = "";
-        $scope.totalValue = 0;
-
-        $scope.calculateTotal = function () {
-            if (angular.isUndefined($scope.name)) {
-                return;
-            }
-
-            $scope.totalValue = 0;
-
-            var i;
-            for (i = 0; i < $scope.name.length; i++) {
-                $scope.totalValue += $scope.name.charCodeAt(i);
-            }
-        };
-
-        $scope.click = function() {
-            return 'click function was called';
-        };
-
-    });
-
-})(window.angular);
+})();
