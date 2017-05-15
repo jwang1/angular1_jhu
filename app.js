379eaa8cfe00837992db27f098aad54f3b620fdf
jwang1 <sigkdd@yahoo.com>
2017-05-14 22:50:39 -0400

    filter injection

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 (function(angular) {
-    angular.module('MyExpressionInterpolationApp', [])
-    .controller('MyExpressionInterpolationController', MyExpressionInterpolationController);
+    angular.module('MyFilterApp', [])
+    .controller('MyFilterController', MyFilterController);
 
-    MyExpressionInterpolationController.$inject = ['$scope', '$filter', '$injector'];
+    MyFilterController.$inject = ['$scope', '$filter', '$injector'];
 
-    function MyExpressionInterpolationController($scope, $filter, $injector) {
+    function MyFilterController($scope, $filter, $injector) {
         $scope.size = 'small';
+
+        $scope.rate = .53;
+
+        $scope.makeNameUppercase = function() {
+            if ($scope.name) {
+                $scope.nameUppercase = $filter('uppercase')($scope.name);
+            }
+        };
 
         $scope.toggleSize = function() {
             if ($scope.size == 'small') {
